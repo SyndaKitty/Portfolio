84e20cd06a3e5051e00f1ba2edc1ff36c4d45951
Spencer Haney <SpencasaurusRex@gmail.com>
2019-09-21 02:05:16 -0400

    Remove unnecessary object-spread plugin
    Babel supports the feature starting with ES6

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread' ]
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
