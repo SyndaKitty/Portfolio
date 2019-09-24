56c5437d8a439d5e2e588b55e1757a34025b3aff
SpencasaurusRex <SpencasaurusRex@gmail.com>
2019-09-23 20:39:54 -0400

    Change main component to presented for all pages

--- app/app.jsx
+++ app/app.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
 import Main from 'Main';
-import Nav from 'Nav';
-import About from 'About';
-import Examples from 'Examples';
 
 ReactDOM.render(
-  <Router>
-    <Nav></Nav>
-    <Route exact path="/" component={Main} />
-    <Route path="/about" component={About} />
-    <Route path="/examples" component={Examples} />
-  </Router>,
+  <Main/>,
   document.getElementById('app')
 )
--- app/components/main.jsx
+++ app/components/main.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Nav from 'Nav';
+import About from 'About';
+import Examples from 'Examples';
 import Weather from 'Weather';
 
 export default class Main extends React.Component {
   render() {
     return (
       <div>
-        <Weather></Weather>
+        <p>Main Comnponent</p>
+        <Router>
+          <Nav></Nav>
+          <Route path="/" exact component={Weather} />
+          <Route path="/weather" component={Weather} />
+          <Route path="/about" component={About} />
+          <Route path="/examples" component={Examples} />
+        </Router>
       </div>
-    );
+    )
   }
 }
